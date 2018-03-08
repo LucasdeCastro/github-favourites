0b0df206fcae4340a3eb028f0f64aa71b39984ff
lucasdecastro <lucascastro102@gmail.com>
2018-03-08 10:31:34 -0300

    Connect repo container with reducer

--- src/containers/Repo.js
+++ src/containers/Repo.js
@@ -1,22 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
-import { selectLang } from "../actions/langActions";
 import { withRouter } from "react-router-dom";
+import { fetchRepo } from "../actions/repoActions";
+import { selectLang } from "../actions/langActions";
 
 class Repo extends React.Component {
   componentDidMount() {
-    const { lang: { selected }, match: { params: { id } } } = this.props;
+    const {
+      repo: { page },
+      lang: { selected },
+      match: { params: { id } }
+    } = this.props;
 
-    console.log(selected, this.props.match);
     if (selected !== id) this.props.selectLang(id);
+    this.props.fetchRepo(id, page);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { repo: { page }, match: { params: { id } } } = this.props;
+    if (prevProps.match.params.id !== id) this.props.fetchRepo(id, page);
   }
 
   render() {
-    const { match } = this.props;
-    return <div>Lang: {match.params.id}</div>;
+    const { match, repo: { total, loading } } = this.props;
+    return (
+      <div>
+        Lang: {match.params.id}, total: {total}, loading:{" "}
+        {loading ? "true" : "false"}
+      </div>
+    );
   }
 }
 
-const mapProps = ({ lang }) => ({ lang });
-const connected = connect(mapProps, { selectLang })(Repo);
+const mapProps = ({ lang, repo }) => ({ lang, repo });
+const connected = connect(mapProps, { selectLang, fetchRepo })(Repo);
 export default withRouter(connected);
