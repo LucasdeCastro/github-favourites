286d3e482daee4702e089bf0d7d6eb559fee4fc6
Lucas de Castro <lucascastro102@gmail.com>
2018-03-10 13:39:30 -0300

    bugfix - change type of dispatch on removeFavourite

--- src/actions/favouriteActions.js
+++ src/actions/favouriteActions.js
@@ -5,5 +5,5 @@ export function addFavourite(item) {
 }
 
 export function removeFavourite(item) {
-  return dispatch => dispatch({ type: ADD_FAVOURITE, payload: [item] });
+  return dispatch => dispatch({ type: REMOVE_FAVOURITE, payload: item });
 }
